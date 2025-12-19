a3515b9c3e77575fe1aa45cb6af2af6845a64344
splenphotography <splenphotography@gmail.com>
2025-12-19 16:18:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+}
+
+setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}, 4000); // changes every 4 seconds
