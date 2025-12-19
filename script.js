ad9dfb742ffd38b0c4ebc5cd1a05ec4d85fc27e0
splenphotography <splenphotography@gmail.com>
2025-12-19 09:38:18 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
